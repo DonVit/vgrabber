5efeb3a9e0d8a2ae1dee480db0d36f4b63b91bbe
vitalie.doni <vitalie.doni@fa28987c-3f28-0410-90f7-3700d64e53a7>
2007-05-05 15:17:13 +0000

    test brach

---
+++ branches/1.02/src/vgrabber/client/MessagesPanel.java
@@ -0,0 +1,258 @@
+/*
+ * MessagesPanel.java
+ *
+ * Created on December 4, 2006, 8:28 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package vgrabber.client;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import vgrabber.common.Contact;
+
+
+public class MessagesPanel extends javax.swing.JPanel{
+    private JSplitPane splitpane=null;
+    private JPanel toppanel=null;
+    private JPanel bottompanel=null;
+    
+    private JPanel filterpanel=null;  
+    private javax.swing.JLabel editionlabel=null;
+    private javax.swing.JComboBox editioncombobox=null;
+    private javax.swing.JLabel categorylabel=null;
+    private javax.swing.JComboBox categorycombobox=null;
+    private javax.swing.JLabel regionlabel=null;
+    private javax.swing.JComboBox regioncombobox=null;
+    
+    private JPanel messagespanel=null;
+    private JTable messagestable=null;
+    private javax.swing.JScrollPane messagestablescrollpane=null;
+    
+    private JSplitPane splitpanebottom=null;
+    
+    private JPanel contactspanel=null;
+    private JTable contactstable=null;
+    private javax.swing.JScrollPane contactstablescrollpane=null;
+    
+    private JPanel contactmessagespanel=null;
+    private JTable contactmessagestable=null;    
+    private javax.swing.JScrollPane contactmessagestablescrollpane=null;
+    
+    private ArrayList<vgrabber.common.Message> messages=null;
+    private ArrayList<vgrabber.common.Contact> contacts=null;
+    private String title=null;
+
+    public MessagesPanel() {
+        super(new GridLayout(1,0));        
+        this.init();           
+    }    
+
+    public void init(){  
+        //Filter Panel        
+        //SpringLayout filterPanelLayout=new javax.swing.SpringLayout();
+        //JPanel filterpanel1=new JPanel(new java.awt.BorderLayout());                
+        //JPanel filterpanel=new JPanel(new java.awt.BorderLayout());                
+        //filterpanel=new JPanel(filterPanelLayout);                
+        filterpanel=new JPanel(new GridLayout(0,8));                        
+        //filterpanel.setSize(50, 500);
+        filterpanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Filtru:"));   
+        //filterPanelLayout.putConstraint(SpringLayout.SOUTH,10,filterpanel,SpringLayout.SOUTH);
+        
+        //Edition
+        editionlabel=new JLabel("Editie: ");
+        editionlabel.setHorizontalAlignment(javax.swing.JLabel.RIGHT);        
+        //SpringLayout.Constraints editionlabelCons = filterPanelLayout.getConstraints(editionlabel);
+        //editionlabelCons.setX(Spring.constant(5));
+        //editionlabelCons.setY(Spring.constant(1));                
+        filterpanel.add(this.editionlabel);        
+        
+        this.editioncombobox=new JComboBox();        
+        for (vgrabber.common.Edition ed:vgrabber.db.EditionManager.GetAllEditions()){
+            this.editioncombobox.addItem(ed);
+        }        
+        this.editioncombobox.addActionListener(new ComboBoxOnChange(this));        
+        //SpringLayout.Constraints editioncomboboxCons = filterPanelLayout.getConstraints(editioncombobox);
+        //editioncomboboxCons.setX(Spring.sum(Spring.constant(5), editionlabelCons.getConstraint(SpringLayout.EAST)));
+        
+        //editioncomboboxCons.setHeight(Spring.constant(50));
+        //editioncomboboxCons.setY(Spring.constant(1));                
+
+        
+        //SpringLayout.Constraints pCons = layout.getConstraints(filterpanel);
+
+        //SpringLayout.Constraints filterpanelCons = filterPanelLayout.getConstraint(filterpanel);
+        //filterpanelCons.setY(100);
+        
+        
+        this.filterpanel.add(this.editioncombobox);
+        //filterpanel.add(this.filterpanel);
+        
+        
+
+
+        
+        
+        //Category
+        this.categorylabel=new JLabel("Categorie: ");
+        this.categorylabel.setHorizontalAlignment(javax.swing.JLabel.RIGHT);        
+        this.filterpanel.add(this.categorylabel);
+        this.categorycombobox=new JComboBox();
+        for (vgrabber.common.Category category:vgrabber.db.CategoryManager.getCategoriesToDownload()){
+            this.categorycombobox.addItem(category);
+        }        
+        this.filterpanel.add(this.categorycombobox);
+        
+        //Region
+        this.regionlabel=new JLabel("Regiune: ");
+        this.regionlabel.setHorizontalAlignment(javax.swing.JLabel.RIGHT);        
+        this.filterpanel.add(this.regionlabel);
+        this.regioncombobox=new JComboBox();
+        for (vgrabber.common.Region region:vgrabber.db.RegionManager.getRegions()){
+            this.regioncombobox.addItem(region);
+        }        
+        this.filterpanel.add(this.regioncombobox);
+        
+                
+        //Messages Panel        
+        this.messagespanel=new JPanel(new GridLayout(1,0));
+        this.messagespanel.setBorder(BorderFactory.createTitledBorder("Anunturi:"));                
+        this.messagestable=new JTable();                
+        
+        this.messagestablescrollpane=new JScrollPane(this.messagestable);                        
+        this.messagestablescrollpane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);                
+        this.messagespanel.add(this.messagestablescrollpane);
+        
+        this.messagestable.getSelectionModel().addListSelectionListener(new MessageSelectionListener(this));
+        
+        //Contacts Panel
+        this.contactspanel=new JPanel(new GridLayout(1,0));
+        this.contactspanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Contacte:"));        
+        this.contactstable=new JTable();                                
+        this.contactstable.getSelectionModel().addListSelectionListener(new ContactSelectionListener(this));
+        
+                                                        
+        this.contactstablescrollpane=new JScrollPane(this.contactstable);                        
+        this.contactstablescrollpane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);                
+        this.contactspanel.add(this.contactstablescrollpane);
+                        
+        
+        //Contact Messages Panel
+        this.contactmessagespanel=new JPanel(new GridLayout(1,0));
+        this.contactmessagespanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Anunturi per Contact:"));        
+        //this.contactmessagestable=new JTable(new makler.client.MessagesTableModel(messages));                                
+        this.contactmessagestable=new JTable();                                     
+        
+        this.contactmessagestablescrollpane=new JScrollPane(this.contactmessagestable);                        
+        this.contactmessagestablescrollpane.setVerticalScrollBarPolicy(javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);                
+        this.contactmessagespanel.add(this.contactmessagestablescrollpane);
+                       
+        
+        this.toppanel=new javax.swing.JPanel(new java.awt.BorderLayout());
+        this.toppanel.add(filterpanel, BorderLayout.NORTH);
+        this.toppanel.add(this.messagespanel, BorderLayout.CENTER);
+        
+        //this.toppanel=new javax.swing.JPanel(new javax.swing.SpringLayout());
+        //this.toppanel.add(filterpanel);
+        //this.toppanel.add(this.messagespanel);
+        
+
+        //this.toppanel=new javax.swing.JPanel();
+        //this.toppanel.add(filterpanel);
+        //this.toppanel.add(this.messagespanel);
+        
+        //this.toppanel=new javax.swing.JPanel(new GridLayout(0,1));
+        //this.toppanel.add(filterpanel);
+        //this.toppanel.add(this.messagespanel);
+
+        
+        this.splitpanebottom=new javax.swing.JSplitPane(javax.swing.JSplitPane.VERTICAL_SPLIT);
+        this.splitpanebottom.setTopComponent(this.contactspanel);
+        this.splitpanebottom.setBottomComponent(this.contactmessagespanel);
+        this.splitpanebottom.setResizeWeight(0.2);
+                
+        this.splitpane=new javax.swing.JSplitPane(javax.swing.JSplitPane.VERTICAL_SPLIT);
+        this.splitpane.setTopComponent(this.toppanel);
+        this.splitpane.setBottomComponent(this.splitpanebottom);
+        this.splitpane.setResizeWeight(0.5);
+        
+        this.fillMessagesTable();
+        
+        this.add(this.splitpane);        
+    }
+
+    private void fillMessagesTable(){
+        this.messages=vgrabber.db.MessageManager.GetMessagesByEditon(((vgrabber.common.Edition)editioncombobox.getSelectedItem()));                                
+        this.messagestable.setModel(new vgrabber.client.MessagesTableModel(messages));
+        this.messagestable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);        
+        this.messagestable.getColumnModel().getColumn(0).setMaxWidth(60);                       
+        this.messagestable.getColumnModel().getColumn(1).setMaxWidth(60);          
+        this.messagestable.getColumnModel().getColumn(2).setMaxWidth(60);        
+        this.messagestable.getSelectionModel().setSelectionInterval(0,0);
+    }
+    private void fillContactsTable(vgrabber.common.Message message){   
+        this.contacts=message.getContacts();
+        this.contactstable.setModel(new vgrabber.client.ContactsTableModel(this.contacts));
+        this.contactstable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        this.contactstable.getColumnModel().getColumn(0).setMaxWidth(80);                                  
+    }    
+    private void fillContactMessagesTable(vgrabber.common.Contact contact){            
+         this.contactmessagestable.setModel(new vgrabber.client.ContactMessagesTabelModel(vgrabber.db.MessageManager.GetMessagesByContact(contact)));
+         this.contactmessagestable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);        
+         this.contactmessagestable.getColumnModel().getColumn(0).setMaxWidth(80);                       
+         this.contactmessagestable.getColumnModel().getColumn(1).setMaxWidth(80);      
+         this.contactmessagestable.getColumnModel().getColumn(2).setMaxWidth(80);      
+    }     
+    class MessageSelectionListener implements javax.swing.event.ListSelectionListener {
+        private MessagesPanel msgp;
+        public MessageSelectionListener(MessagesPanel msgp){
+            this.msgp=msgp;
+        }
+        public void valueChanged(ListSelectionEvent e){
+                    if (e.getValueIsAdjusting()) return;
+
+                    ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+                    if (lsm.isSelectionEmpty()) {
+                        System.out.println("No rows are selected.");
+                    } else {                        
+                        int selectedRow = lsm.getMinSelectionIndex();                        
+                        msgp.fillContactsTable(msgp.messages.get(selectedRow));                        
+                        msgp.contactstable.getSelectionModel().setSelectionInterval(0,0);
+                        int value=msgp.messages.get(selectedRow).getId();                        
+                        System.out.println("Row " + selectedRow+ " is now selected. ID is "+value);            
+                    }
+        }
+    }
+    class ContactSelectionListener implements javax.swing.event.ListSelectionListener {
+        private MessagesPanel msgp;
+        public ContactSelectionListener(MessagesPanel msgp){
+            this.msgp=msgp;
+            
+        }
+        public void valueChanged(ListSelectionEvent e){
+                    if (e.getValueIsAdjusting()) return;
+
+                    ListSelectionModel lsm = (ListSelectionModel)e.getSource();
+                    if (lsm.isSelectionEmpty()) {
+                        System.out.println("No rows are selected.");
+                    } else {                        
+                        int selectedRow = lsm.getMinSelectionIndex();                        
+                        msgp.fillContactMessagesTable(msgp.contacts.get(selectedRow));                        
+                        msgp.contactmessagestable.getSelectionModel().setSelectionInterval(0,0);
+                    }
+        }
+    }    
+    class ComboBoxOnChange implements java.awt.event.ActionListener{
+        private MessagesPanel msgp;
+        public ComboBoxOnChange(MessagesPanel msgp){
+            this.msgp=msgp;            
+        }
+        public void actionPerformed(ActionEvent e) {
+            this.msgp.fillMessagesTable();
+        }
+    }                 
+}
---
+++ branches/1.02/src/vgrabber/db/MessageManager.java
@@ -0,0 +1,218 @@
+/*
+ * MessageManager.java
+ *
+ * Created on November 24, 2006, 3:19 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package vgrabber.db;
+
+import java.util.*;
+import java.sql.*;
+import vgrabber.common.Contact;
+import vgrabber.common.Edition;
+import vgrabber.common.Message;
+
+/**
+ *
+ * @author vdoni
+ */
+public class MessageManager {
+    
+    /** Creates a new instance of MessageManager */
+    public MessageManager() {
+    }
+    public static ArrayList<Message> GetMessage(int id){
+        ArrayList<Message> messages=new ArrayList<Message>();
+        try{        
+        String sql="SELECT id, message, editie_id, categorie_id, interested FROM anunt WHERE (id = ?)";
+        java.sql.PreparedStatement psMessages=vgrabber.db.Connection.GetConnection().prepareStatement(sql);
+        psMessages.setInt(1,id);
+        psMessages.execute();
+        java.sql.ResultSet rsMessages=psMessages.getResultSet();        
+        while (rsMessages.next()){        
+        Message message=new Message(rsMessages.getInt("id"),rsMessages.getString("anunt"),rsMessages.getInt("editie_id"),rsMessages.getInt("categorie_id"),rsMessages.getBoolean("interested"));        
+        messages.add(message);
+        }
+        }
+        catch (java.sql.SQLException ex){
+            System.out.println(ex.toString());
+        }
+        finally{            
+            return messages;
+        }
+    }
+    public static ArrayList<Message> GetMessagesByContact(Contact contact){
+        ArrayList<Message> messages=new ArrayList<Message>();
+        try{        
+        String sql="SELECT anunt.id, editie_id, categorie_id, anunt, interested FROM anunt INNER JOIN contact_anunt ON anunt.id = contact_anunt.anunt_id WHERE (contact_anunt.contact_id = ?)";
+        java.sql.PreparedStatement psMessages=vgrabber.db.Connection.GetConnection().prepareStatement(sql);
+        psMessages.setString(1,contact.getId());
+        psMessages.execute();
+        java.sql.ResultSet rsMessages=psMessages.getResultSet();
+        while (rsMessages.next()){        
+        Message message=new Message(rsMessages.getInt("id"),rsMessages.getString("anunt"),rsMessages.getInt("editie_id"),rsMessages.getInt("categorie_id"),rsMessages.getBoolean("interested"));        
+        messages.add(message);
+        }
+        }
+        catch (java.sql.SQLException ex){
+            System.out.println(ex.toString());
+        }
+        finally{            
+            return messages;
+        }
+    }    
+    public static ArrayList<Message> GetMessagesByEditon(Edition edition){
+        ArrayList<Message> messages=new ArrayList<Message>();
+        try{        
+        String sql="SELECT id, categorie_id, anunt, interested FROM anunt WHERE editie_id = ?";
+        java.sql.PreparedStatement psMessages=vgrabber.db.Connection.GetConnection().prepareStatement(sql);
+        psMessages.setInt(1,edition.getId());
+        psMessages.execute();
+        java.sql.ResultSet rsMessages=psMessages.getResultSet();
+        while (rsMessages.next()){        
+        Message message=new Message(rsMessages.getInt("id"),rsMessages.getString("anunt"),edition.getId(),rsMessages.getInt("categorie_id"),rsMessages.getBoolean("interested"));        
+        messages.add(message);
+        }
+        }
+        catch (java.sql.SQLException ex){
+            System.out.println(ex.toString());
+        }
+        finally{            
+            return messages;
+        }
+    }    
+    public static ArrayList<Message> GetNewMessagesByEditon(Edition edition){
+        ArrayList<Message> messages=new ArrayList<Message>();
+        try{        
+        String sql="select tb1.id, (select categorie_id from anunt where id=tb1.id)as categorie_id, (select anunt from anunt where id=tb1.id )as anunt, (select interested from anunt where id=tb1.id )as interested from (select anunt.id, contact_anunt.contact_id from anunt inner join contact_anunt on anunt.id=contact_anunt.anunt_id where editie_id=?) tb1 where tb1.contact_id not in (select contact_anunt.contact_id from anunt inner join contact_anunt on anunt.id=contact_anunt.anunt_id where editie_id<>?) group by tb1.id";        
+        java.sql.PreparedStatement psMessages=vgrabber.db.Connection.GetConnection().prepareStatement(sql); 
+        psMessages.setInt(1,edition.getId());
+        psMessages.setInt(2,edition.getId());        
+        psMessages.execute();
+            
+        java.sql.ResultSet rsMessages=psMessages.getResultSet();
+        while (rsMessages.next()){        
+        Message message=new Message(rsMessages.getInt("id"),rsMessages.getString("anunt"),edition.getId(),rsMessages.getInt("categorie_id"),rsMessages.getBoolean("interested"));        
+        messages.add(message);
+        }
+        }
+        catch (java.sql.SQLException ex){
+            System.out.println(ex.toString());
+        }
+        finally{            
+            return messages;
+        }
+    }        
+    public static ArrayList<Message> GetFavoriteMessages(){
+        ArrayList<Message> messages=new ArrayList<Message>();
+        try{        
+        String sql="SELECT id, editie_id, categorie_id, anunt,interested FROM anunt WHERE interested = 1";
+        java.sql.PreparedStatement psMessages=vgrabber.db.Connection.GetConnection().prepareStatement(sql);        
+        psMessages.execute();
+        java.sql.ResultSet rsMessages=psMessages.getResultSet();
+        while (rsMessages.next()){        
+        Message message=new Message(rsMessages.getInt("id"),rsMessages.getString("anunt"),rsMessages.getInt("editie_id"),rsMessages.getInt("categorie_id"),rsMessages.getBoolean("interested"));        
+        messages.add(message);
+        }
+        }
+        catch (java.sql.SQLException ex){
+            System.out.println(ex.toString());
+        }
+        finally{            
+            return messages;
+        }
+    }            
+    public static Message AddMessage(Message message){                
+        try{        
+        String sql="INSERT INTO anunt (editie_id, categorie_id, anunt) VALUES (?, ?, ?)";
+        java.sql.PreparedStatement psMessage=vgrabber.db.Connection.GetConnection().prepareStatement(sql);
+        psMessage.setInt(1,message.getEdition_id());
+        psMessage.setInt(2,message.getCategory_id());
+        psMessage.setString(3,message.getAnunt());
+        psMessage.execute();
+        sql="select top 1 id from anunt order by id desc";
+        java.sql.PreparedStatement psMessageLastId=vgrabber.db.Connection.GetConnection().prepareStatement(sql);
+        psMessageLastId.execute();
+        java.sql.ResultSet rsMessageLastId=psMessageLastId.getResultSet();
+        while (rsMessageLastId.next()){
+            message.setId(rsMessageLastId.getInt("id"));
+        }        
+        
+        // Add Message Contacts           
+        for (Contact contact:message.getContacts()){
+            if (!vgrabber.db.ContactManager.ContactExists(contact))
+            vgrabber.db.ContactManager.AddContact(contact);
+            sql="INSERT INTO contact_anunt (contact_id, anunt_id) VALUES (?,?)";
+            java.sql.PreparedStatement psContact=vgrabber.db.Connection.GetConnection().prepareStatement(sql);
+            psContact.setString(1,contact.getId());
+            psContact.setInt(2,message.getId());            
+            psContact.execute();            
+        }
+        
+        }
+        catch (java.sql.SQLException ex){
+            System.out.println(ex.toString());
+        }
+        finally{            
+            return message;
+        }        
+    }
+    public static boolean UpdMessage(Message message){        
+        boolean updated=false;
+        try{        
+        String sql="UPDATE anunt SET editie_id =?, categorie_id =?, anunt =?, interested=? WHERE (id = ?)";
+        java.sql.PreparedStatement psMessage=vgrabber.db.Connection.GetConnection().prepareStatement(sql);                
+        psMessage.setInt(1,message.getEdition_id());        
+        psMessage.setInt(2,message.getCategory_id());
+        psMessage.setString(3,message.getAnunt());
+        psMessage.setBoolean(4,message.getInterested());        
+        psMessage.setInt(5,message.getId());
+        psMessage.execute();
+
+        //Remove Message Contacts
+        sql="DELETE contact_anunt WHERE anunt_id=?";
+        java.sql.PreparedStatement psContactRem=vgrabber.db.Connection.GetConnection().prepareStatement(sql);            
+        psContactRem.setInt(1,message.getId());            
+        psContactRem.execute();            
+        
+        // Add Message Contacts           
+        for (Contact contact:message.getContacts()){
+            if (!vgrabber.db.ContactManager.ContactExists(contact))
+            vgrabber.db.ContactManager.AddContact(contact);
+            sql="INSERT INTO contact_anunt (contact_id, anunt_id) VALUES (?,?)";
+            java.sql.PreparedStatement psContact=vgrabber.db.Connection.GetConnection().prepareStatement(sql);
+            psContact.setString(1,contact.getId());
+            psContact.setInt(2,message.getId());            
+            psContact.execute();            
+        }        
+        
+        updated=true;
+        }
+        catch (java.sql.SQLException ex){
+            System.out.println(ex.toString());
+        }
+        finally{            
+            return updated;
+        }        
+    }
+    public static boolean DelMessage(Message message){        
+        boolean deleted=false;
+        try{        
+        String sql="delete anunt WHERE (id = ?)";
+        java.sql.PreparedStatement psMessage=vgrabber.db.Connection.GetConnection().prepareStatement(sql);        
+        psMessage.setInt(1,message.getId());
+        psMessage.execute();
+        deleted=true;
+        }
+        catch (java.sql.SQLException ex){
+            System.out.println(ex.toString());
+        }
+        finally{            
+            return deleted;
+        }        
+    }
+    
+}
